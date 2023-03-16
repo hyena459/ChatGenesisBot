a429592b1c0775d993615dd2f1da95ab31aab3bb
hy3na <hyena459@gmail.com>
2023-03-16 10:20:24 +0900

    add-logging

--- slack_bot.py
+++ slack_bot.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import openai
 from slack_bolt import App
@@ -5,27 +6,29 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 app = App()
+logging.basicConfig(level=logging.DEBUG)
 
 @app.event("app_mention")
 async def command_handler(body, say):
     text = body['event']['text']
     user = body['event']['user']
+    logging.debug(f"Received a message: {text}")
 
     # メンションを取り除く
     prompt = text.replace(f'<@{user}>', '').strip()
 
     # GPT-3.5-turboを使ってリクエストを生成
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="chatgpt-3.5-turbo",
         prompt=prompt,
         max_tokens=150,
         n=1,
         stop=None,
         temperature=0.5,
     )
-
     # 返信を取得し、Slackに送信
     reply = response.choices[0].text.strip()
+    logging.debug(f"Replyed a message: {reply}")
     await say(f'<@{user}> {reply}')
 
 if __name__ == "__main__":
