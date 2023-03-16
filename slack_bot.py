e275e59ba3aa7547ec9c5f70eaf9db649ce18e53
hy3na <hyena459@gmail.com>
2023-03-16 10:37:04 +0900

    test

--- slack_bot.py
+++ slack_bot.py
@@ -9,28 +9,32 @@ app = App()
 logging.basicConfig(level=logging.DEBUG)
 
 @app.event("app_mention")
-async def command_handler(body, say):
-    logging.debug("Received app_mention event")
-    text = body['event']['text']
-    user = body['event']['user']
-    logging.debug(f"Received a message: {text}")
+async def test_handler(body, say):
+    print("Test handler triggered")
+    await say("Hello, I'm here!")
 
-    # メンションを取り除く
-    prompt = text.replace(f'<@{user}>', '').strip()
+# async def command_handler(body, say):
+#     logging.debug("Received app_mention event")
+#     text = body['event']['text']
+#     user = body['event']['user']
+#     logging.debug(f"Received a message: {text}")
 
-    # GPT-3.5-turboを使ってリクエストを生成
-    response = openai.Completion.create(
-        engine="chatgpt-3.5-turbo",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    # 返信を取得し、Slackに送信
-    reply = response.choices[0].text.strip()
-    logging.debug(f"Replyed a message: {reply}")
-    await say(f'<@{user}> {reply}')
+#     # メンションを取り除く
+#     prompt = text.replace(f'<@{user}>', '').strip()
+
+#     # GPT-3.5-turboを使ってリクエストを生成
+#     response = openai.Completion.create(
+#         engine="chatgpt-3.5-turbo",
+#         prompt=prompt,
+#         max_tokens=150,
+#         n=1,
+#         stop=None,
+#         temperature=0.5,
+#     )
+#     # 返信を取得し、Slackに送信
+#     reply = response.choices[0].text.strip()
+#     logging.debug(f"Replyed a message: {reply}")
+#     await say(f'<@{user}> {reply}')
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
