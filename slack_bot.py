7567eea76ef4727ebe3c4b4cfabe795aee27cc54
hy3na <hyena459@gmail.com>
2023-03-16 18:52:31 +0900

    save-talk-history

--- slack_bot.py
+++ slack_bot.py
@@ -1,47 +1,66 @@
 import logging
-import os
 import openai
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
-logging.basicConfig(level=logging.DEBUG)
+
+# Add the functions for managing messages and tokens
+def add_message(messages, role, content, max_tokens):
+    new_message = {"role": role, "content": content}
+    messages.append(new_message)
+
+    while total_tokens(messages) > max_tokens:
+        messages.pop(0)  # Remove the oldest message
+
+    return messages
+
+def total_tokens(messages):
+    tokens = 0
+    for message in messages:
+        tokens += len(message["content"])  # Estimate the number of tokens
+    return tokens
+
+# Initialize an empty messages list and set your desired max_tokens limit
+messages = []
+max_tokens = 4096
+
+# Add the initial system message
+initial_system_message = "You are an excellent butler in our family. YourName is 風間 You are 56 years old, intelligent, gentlemanly and calm. You are often charming."
+messages = add_message(messages, "system", initial_system_message, max_tokens)
 
 @app.event("app_mention")
 def mention_handler(body, say):
-    logging.debug("Received app_mention event")
     text = body['event']['text']
     user = body['event']['user']
-    logging.debug(f"Received a message: {text}")
 
     # メンションを取り除く
     prompt = text.replace(f'<@{user}>', '').strip()
 
     try:
+        # Add the user's message to the messages list
+        messages = add_message(messages, "user", prompt, max_tokens)
+
         # GPT-3.5-turboを使ってリクエストを生成
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are an excellent butler in our family. YourName is 風間 You are 56 years old, intelligent, gentlemanly and calm."},
-                {"role": "user", "content": prompt}
-                ],
+            messages=messages,
+            temperature=0.8,
+            max_tokens=80,
+            stream=True,
         )
+
         # 返信を取得し、Slackに送信
         reply = response.choices[0]["message"]["content"].strip()
         logging.debug(f"Reply a message: {reply}")
         say(f'<@{user}> {reply}')
+
+        # Add the assistant's reply to the messages list
+        messages = add_message(messages, "assistant", reply, max_tokens)
+
     except Exception as e:
         logging.error(f"Error: {e}")
 
-# @app.event("app_mention")
-# def mention_handler(body, say):
-#     print("Test handler triggered")
-#     try:
-#         say("Hello, I'm here!")
-#     except Exception as e:
-#         print(f"Error: {e}")
-
-if __name__ == "__main__":
-    handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
-    handler.start()
+handler = SocketModeHandler(app, "SLACK_APP_TOKEN")
+handler.start()
